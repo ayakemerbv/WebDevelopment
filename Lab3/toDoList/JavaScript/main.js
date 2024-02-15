e47090b2f313b0f17c4127284092d35338d995b2
ayakemerbv <ayakemer@mail.ru>
2024-02-15 23:54:14 +0600

    third commit

---
+++ Lab3/toDoList/JavaScript/main.js
@@ -0,0 +1,41 @@
+
+var cnt = 1;
+var last = null;
+function saveTag(el){
+    last = el.value;
+}
+function buttonSave(el){
+    createList(last);
+}
+
+
+function printArr(el){
+    console.log(arr);
+}
+
+function createList(last){
+    var newDiv = document.createElement("div"); // создаем новый элемент div
+    newDiv.className = cnt+"chek";
+    cnt++;
+    var save = '<input id = "bx" onclick = "overLine(this)" type="checkbox"><p class = "Tags" >'+last+'</p><button onclick = "deleteById(this)" class = "del"></button>';
+    newDiv.innerHTML = save; // задаем содержимое элемента
+    document.body.appendChild(newDiv); // добавляем новый элемент в тело документа
+}
+
+function deleteById(el){
+    const parent = el.parentNode;
+
+    parent.remove();
+
+}
+
+function overLine(el){
+    const parent = el.parentNode;
+    const p = parent.querySelector("p");
+    if(p.className=="NewClass"){
+        p.className = 'Tags';
+    }
+    else{
+        p.className = 'NewClass';
+    }
+}
